3d7ca96e16b8df0ff62f21a2730ebb7dcd239fe0
Paul Bates <paul.a.bates@gmail.com>
2018-03-09 16:31:01 -0800

    Inline source maps

--- dist/index.js
+++ dist/index.js
@@ -6,4 +6,4 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const tslib_1 = require("tslib");
 tslib_1.__exportStar(require("./lib/decoder-map"), exports);
 tslib_1.__exportStar(require("./lib/json/json-decoder"), exports);
-//# sourceMappingURL=index.js.map
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9pbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiO0FBQUE7O0dBRUc7OztBQUVILDREQUFpQztBQUNqQyxrRUFBdUMifQ==
